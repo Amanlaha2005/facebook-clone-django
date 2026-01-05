1e3565f109b54bcbc4fb63fe50f8985e2c301f6f
Aman Laha <amanlaha2005@gmail.com>
2026-01-05 13:41:52 +0530

    adding for render

--- FaceBookClone/settings.py
+++ FaceBookClone/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = 'django-insecure-change-this-later'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # =========================
